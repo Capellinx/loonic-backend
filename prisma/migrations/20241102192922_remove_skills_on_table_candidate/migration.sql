1e1e4c2bc185b8f982c8395dbb75d3fbb912e819
capellinx <capellaaa7@gmail.com>
2024-11-02 16:30:21 -0300

    feat: create new migration remove skills

---
+++ prisma/migrations/20241102192922_remove_skills_on_table_candidate/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `skills` on the `candidates` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "candidates" DROP COLUMN "skills";
