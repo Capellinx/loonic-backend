eb7db4114f129ea968aa94b181ac803805ed2e24
capellinx <capellaaa7@gmail.com>
2024-11-02 16:59:10 -0300

    feat: add new validation body

--- src/routes/candidate.routes.ts
+++ src/routes/candidate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { craeteCandidateController } from "../use-cases/candidate";
+import { ZodRequestValidate } from "../middleware/zod-errors.middleware";
+import { createCandidateSchema } from "../use-cases/candidate/create-candidate-dto";
 
 
 export const candidateRouter = Router()
 
 candidateRouter.post(
    "/candidate",
+   ZodRequestValidate.execute({body: createCandidateSchema}),
    async (request, response) => {
       await craeteCandidateController.handle(request, response)
    }
