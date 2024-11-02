80ba185150e7741dc740e9b3264eac5147832f53
capellinx <capellaaa7@gmail.com>
2024-11-02 20:22:33 -0300

    fix: change name class

--- src/infra/services/bcrypt.service.ts
+++ src/infra/services/bcrypt.service.ts
@@ -1,11 +1,11 @@
 import { EncryptService } from "../../domain/services/encrypt.service";
 import bcrypt from 'bcrypt';
 
-export class BcryptLogin implements EncryptService {
+export class BcryptService implements EncryptService {
    private static readonly SALT_ROUNDS = 10
 
    async hashPassword(password: string): Promise<string> {
-      return await bcrypt.hash(password, BcryptLogin.SALT_ROUNDS)
+      return await bcrypt.hash(password, BcryptService.SALT_ROUNDS)
    }
 
    async comparePassword(password: string, passwordHash: string): Promise<boolean> {
