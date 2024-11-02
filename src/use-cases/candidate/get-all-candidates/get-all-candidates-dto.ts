c75ccee5e1d642746aeb6e973ba4047675fd0eba
capellinx <capellaaa7@gmail.com>
2024-11-02 19:04:57 -0300

    feat: update schema to validate serch

--- src/use-cases/candidate/get-all-candidates/get-all-candidates-dto.ts
+++ src/use-cases/candidate/get-all-candidates/get-all-candidates-dto.ts
@@ -2,7 +2,14 @@ import { z } from "zod";
 
 
 export const getAllCandidatesSchema = z.object({
-   id: z.string(),
+   page: z.coerce.number().default(1).optional(),
+   name: z.string().min(1).optional(),
+   status: z.enum([
+      "DISPONIVEL",
+      "INDISPONIVEL",
+      "EM_PROCESSO"
+   ]).optional(),
+   skills: z.array(z.string()).optional(),
 })
 
 export type GetAllCandidatesDTO = z.infer<typeof getAllCandidatesSchema>
