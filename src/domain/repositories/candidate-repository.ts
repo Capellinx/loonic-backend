1c11b53a86c2ed2bd7dd77abbf626b5f66787a84
capellinx <capellaaa7@gmail.com>
2024-11-02 16:30:34 -0300

    fix: change return create repo

--- src/domain/repositories/candidate-repository.ts
+++ src/domain/repositories/candidate-repository.ts
@@ -3,5 +3,5 @@ import { Candidate } from "../entity/candidate";
 
 export interface CandidateRepository {
    findByEmail(email: string): Promise<Candidate | null>
-   create(payload: CreateCandidateDTO): Promise<Candidate | null>
+   create(payload: CreateCandidateDTO): Promise<void>
 }
