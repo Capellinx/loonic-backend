8d88edaf3cd17541003e1363a70e9ee986e7cea6
capellinx <capellaaa7@gmail.com>
2024-11-02 20:31:57 -0300

    feat: implements methods in class manager repo

---
+++ src/infra/repositories/manager-repository-db.ts
@@ -0,0 +1,30 @@
+import { prisma } from "../../../prisma/db";
+import { Manager } from "../../domain/entity/manager";
+import { ManagerRepository } from "../../domain/repositories/manager-repository";
+import { RegisterManagerDTO } from "../../use-cases/manager/register-manager/register-manager-dto";
+
+
+export class ManagerRepositoryDB implements ManagerRepository {
+   async findByEmail(email: string): Promise<Omit<Manager, 'password'> | null> {
+      const manager = await prisma.manager.findUnique({
+         where: {
+            email
+         }
+      })
+
+      if (!manager) return null
+
+      return manager
+   }
+
+   async create({ email, password }: RegisterManagerDTO): Promise<any> {
+      return await prisma.manager.create({
+         data: {
+            email,
+            password
+         }
+      })
+
+   }
+
+}
