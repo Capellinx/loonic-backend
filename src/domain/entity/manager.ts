e11eb752e975008e89e4447134f91cdfe4cf818e
capellinx <capellaaa7@gmail.com>
2024-11-02 20:03:46 -0300

    fix: remove proprety name

--- src/domain/entity/manager.ts
+++ src/domain/entity/manager.ts
@@ -1,7 +1,6 @@
 
 interface ManagerProps {
    id: string
-   name: string
    email: string
    password: string
    createdAt: Date
@@ -10,14 +9,12 @@ interface ManagerProps {
 
 export class Manager {
    public id: string
-   public name: string
    public email: string
    public password: string
    public createdAt: Date
    public updatedAt: Date
    constructor(props: ManagerProps) {
       this.id = props.id
-      this.name = props.name
       this.email = props.email
       this.password = props.password
       this.createdAt = props.createdAt
