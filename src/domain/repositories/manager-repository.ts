0e2b7de66a3ff2cb1a0232a3a6d1b4064d43e6eb
capellinx <capellaaa7@gmail.com>
2024-11-02 20:30:53 -0300

    feat: define typing on parameters create

--- src/domain/repositories/manager-repository.ts
+++ src/domain/repositories/manager-repository.ts
@@ -1,6 +1,7 @@
+import { RegisterManagerDTO } from "../../use-cases/manager/register-manager/register-manager-dto"
 import { Manager } from "../entity/manager"
 
 export interface ManagerRepository {
    findByEmail(email: string): Promise<Omit<Manager, 'password'> | null>
-   create(manager: Manager): Promise<any>
+   create({email, password}: RegisterManagerDTO): Promise<void>
 }
