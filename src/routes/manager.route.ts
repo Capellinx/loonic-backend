543295ed31112152125c0db224a37904879bd40b
capellinx <capellaaa7@gmail.com>
2024-11-02 20:46:43 -0300

    feat: create route to login

--- src/routes/manager.route.ts
+++ src/routes/manager.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { registerManagerSchema } from "../use-cases/manager/register-manager/register-manager-dto";
 import { registerManagerController } from "../use-cases/manager/register-manager";
 import { ZodRequestValidate } from "../middleware/zod-errors.middleware";
+import { loginManagerSchema } from "../use-cases/manager/login-manager/login-manager-dto";
+import { loginManagerController } from "../use-cases/manager/login-manager";
 
 export const managerRouter = Router()
 
@@ -10,5 +12,13 @@ managerRouter.post(
    ZodRequestValidate.execute({ body: registerManagerSchema }),
    async (request, response) => {
       await registerManagerController.handle(request, response)
+   }
+)
+
+managerRouter.post(
+   "/manager/login",
+   ZodRequestValidate.execute({ body: loginManagerSchema }),
+   async (request, response) => {
+      await loginManagerController.handle(request, response)
    }
 )
