e254f61ae46526ae42d48b5c4eb885852f8aec27
capellinx <capellaaa7@gmail.com>
2024-11-02 20:46:24 -0300

    fix: change returning typing

--- src/domain/repositories/manager-repository.ts
+++ src/domain/repositories/manager-repository.ts
@@ -2,6 +2,6 @@ import { RegisterManagerDTO } from "../../use-cases/manager/register-manager/reg
 import { Manager } from "../entity/manager"
 
 export interface ManagerRepository {
-   findByEmail(email: string): Promise<Omit<Manager, 'password'> | null>
+   findByEmail(email: string): Promise<Manager| null>
    create({email, password}: RegisterManagerDTO): Promise<void>
 }
