7f7f1361ca120b36150effebb52e3962c6c3d004
capellinx <capellaaa7@gmail.com>
2024-11-02 19:05:07 -0300

    feat: create new route to get all users

--- src/routes/candidate.routes.ts
+++ src/routes/candidate.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { craeteCandidateController } from "../use-cases/candidate/create-candidate";
 import { ZodRequestValidate } from "../middleware/zod-errors.middleware";
 import { createCandidateSchema } from "../use-cases/candidate/create-candidate/create-candidate-dto";
+import { getAllCandidatesSchema } from '../use-cases/candidate/get-all-candidates/get-all-candidates-dto';
+import { getAllCandidatesController } from "../use-cases/candidate/get-all-candidates";
 
 
 export const candidateRouter = Router()
@@ -11,5 +13,13 @@ candidateRouter.post(
    ZodRequestValidate.execute({body: createCandidateSchema}),
    async (request, response) => {
       await craeteCandidateController.handle(request, response)
+   }
+)
+
+candidateRouter.get(
+   "/candidate",
+   ZodRequestValidate.execute({params: getAllCandidatesSchema}),
+   async (request, response) => {
+      await getAllCandidatesController.handle(request, response)
    }
 )
