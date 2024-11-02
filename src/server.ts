4ee0ff391287bd9bbfe7b8d9cc03c5ccb444dcbe
capellinx <capellaaa7@gmail.com>
2024-11-02 16:50:34 -0300

    feat: create connection with db before start server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
+import { prisma } from "../prisma/db";
 import { app } from "./app";
+import { env } from "./config/env";
 
-app.listen(3333, () => {
-   console.log(`🚀 Server started on port ${3333}`);
-});
+async function connectToDatabase() {
+   try {
+      await prisma.$connect();
+      console.log("🎲 Connected to database");
+   } catch (error) {
+      console.error("❌ Failed to connect to database", error);
+   }
+}
+
+async function startServer() {
+   await connectToDatabase();
+
+   app.listen(env.PORT, () => {
+      console.log(`🚀 Server started on port ${env.PORT}`);
+   });
+
+}
+
+startServer()
