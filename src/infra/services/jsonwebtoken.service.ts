e629dc0bf4c16a8d361ae318b10e76bfbf4d5851
capellinx <capellaaa7@gmail.com>
2024-11-02 19:57:01 -0300

    feat: create service token

---
+++ src/infra/services/jsonwebtoken.service.ts
@@ -0,0 +1,15 @@
+import Jwt from "jsonwebtoken";
+import { TokenService } from "../../domain/services/token.service";
+import { env } from "../../config/env";
+
+export class JsonWebToken implements TokenService {
+   async generateToken(payload: Record<string, any>): Promise<string> {
+      const accessToken = Jwt.sign(payload, env.JWT_SECRET, { expiresIn: '1d' })
+      return accessToken
+   }
+
+   async refreshToken(id: number): Promise<string> {
+      const refreshToken = Jwt.sign({ id }, env.JWT_REFRESH_SECRET, { expiresIn: '7d' })
+      return refreshToken
+   }
+}
