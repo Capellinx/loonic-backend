271941688ebfee438afe7d7295a4cba6c85b26cc
capellinx <capellaaa7@gmail.com>
2024-11-02 16:44:38 -0300

    feat: create implements repo db

---
+++ src/infra/repositories/candidate-repository-db.ts
@@ -0,0 +1,27 @@
+import { prisma } from "../../../prisma/db";
+import { Candidate } from "../../domain/entity/candidate";
+import { CandidateRepository } from "../../domain/repositories/candidate-repository";
+import { CreateCandidateDTO } from "../../use-cases/candidate/create-candidate-dto";
+
+
+export class CandidateRepositoryDB implements CandidateRepository {
+   async findByEmail(email: string): Promise<Candidate | null> {
+      const candidate = await prisma.candidate.findUnique({
+         where: {
+            email,
+         },
+      })
+
+      if (candidate) return candidate
+
+      return null
+   }
+
+   async create(payload: CreateCandidateDTO): Promise<void> {
+      await prisma.candidate.create({
+         data: payload
+      })
+
+      return
+   }
+}
