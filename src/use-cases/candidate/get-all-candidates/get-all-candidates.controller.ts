a9237a47a5d8f14bd0417105ed342fcddcb6d3b9
capellinx <capellaaa7@gmail.com>
2024-11-02 19:04:29 -0300

    feat: create controller to show all users

---
+++ src/use-cases/candidate/get-all-candidates/get-all-candidates.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { GetAllCandidatesUseCase } from "./get-all-candidates";
+import { Satatus } from "@prisma/client";
+import { getAllCandidatesSchema } from "./get-all-candidates-dto";
+
+
+export class GetAllCandidatesController {
+   constructor(
+      private getAllCandidatesUseCase: GetAllCandidatesUseCase
+   ) { }
+
+   async handle(request: Request, response: Response): Promise<Response> {
+      const { name, skills, status, page } = request.query
+
+      const skillsArray = Array.isArray(skills) ? skills : (skills ? [skills] : undefined);
+
+      const params = getAllCandidatesSchema.parse({
+         ...request.query,
+         skills: skillsArray,
+      })
+
+      const data = await this.getAllCandidatesUseCase.execute(params)
+
+      return response.status(200).json(data)
+   }
+}
