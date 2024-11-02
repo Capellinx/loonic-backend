c4107d59556bd14bed4c215b5abacdfe67325cba
capellinx <capellaaa7@gmail.com>
2024-11-02 19:51:57 -0300

    feat: create contract to update

--- src/domain/repositories/candidate-repository.ts
+++ src/domain/repositories/candidate-repository.ts
@@ -1,5 +1,6 @@
 import { CreateCandidateDTO } from "../../use-cases/candidate/create-candidate/create-candidate-dto";
 import { GetAllCandidatesDTO } from "../../use-cases/candidate/get-all-candidates/get-all-candidates-dto";
+import { UpdateCandidateDTO } from "../../use-cases/candidate/update-candidate/update-candidate-dto";
 import { Candidate } from "../entity/candidate";
 
 export interface CandidateRepository {
@@ -7,6 +8,7 @@ export interface CandidateRepository {
    findById(id: string): Promise<Candidate | null>
    delete(id: string): Promise<void>
    create(payload: CreateCandidateDTO): Promise<void>
+   update(id: string, payload: UpdateCandidateDTO): Promise<void>
    getAllCandidates({name, skills, status}: GetAllCandidatesDTO): Promise<[Candidate[], number]>
    getOneCandidate(id: string): Promise<Candidate | null>
 }
