f50ac2a61eb92fad9cadbac89bdfd8e829e88d30
capellinx <capellaaa7@gmail.com>
2024-11-03 16:33:14 -0300

    fix: remove helmet

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import "express-async-errors"
 import express, { json } from "express";
-import helmet from "helmet";
 import cors from "cors"
 import { candidateRouter } from "./routes/candidate.routes";
 import { errorHandler } from "./middleware/handle-errors.middleware";
@@ -9,7 +8,6 @@ import { managerRouter } from "./routes/manager.route";
 const app = express()
 
 app.use(json())
-app.use(helmet())
 app.use(cors({
    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
    origin: "http://localhost:5173",
