63e1557627b68c3ec42cd608e974b21d40ee655d
capellinx <capellaaa7@gmail.com>
2024-11-03 14:15:09 -0300

    feat: typing to docker compose

--- src/use-cases/candidate/create-candidate/create-candidate.ts
+++ src/use-cases/candidate/create-candidate/create-candidate.ts
@@ -3,6 +3,7 @@ import { Candidate } from "../../../domain/entity/candidate";
 import { CandidateRepository } from "../../../domain/repositories/candidate-repository";
 import { NotFoundError } from "../../../config/errors/application-errors";
 import { CreateCandidateDTO } from "./create-candidate-dto";
+import { Status } from "@prisma/client";
 
 export class CreateCandidateUseCase {
 
@@ -11,19 +12,19 @@ export class CreateCandidateUseCase {
    ) { }
 
    async execute(payload: CreateCandidateDTO): Promise<void> {
-      const cadidate = await this.candidateRepository.findByEmail(payload.email)
+      const cadidate = await this.candidateRepository.findByEmail(payload.email as string)
 
       if (cadidate) throw new NotFoundError("Esse candidato já está cadastrado.")
 
-      const newCandidate = new Candidate({
+      const newCandidate: Candidate = new Candidate({
          id: randomUUID(),
-         name: payload.name,
-         email: payload.email,
-         education: payload.education,
-         experience: payload.education,
-         phone: payload.phone,
-         status: payload.status,
-         skills: payload.skills,
+         name: payload.name as string,
+         email: payload.email as string,
+         education: payload.education as string,
+         experience: payload.education as string,
+         phone: payload.phone as string,
+         status: payload.status as Status,
+         skills: payload.skills as { name: string }[],
          createdAt: new Date(),
          updatedAt: new Date()
       })
