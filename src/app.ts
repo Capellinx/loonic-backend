23528ffcd70880fe3560e2c2573573e4e6e85926
capellinx <capellaaa7@gmail.com>
2024-11-03 18:25:22 -0300

    feat: instance jsondoc on app

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import cors from "cors"
 import { candidateRouter } from "./routes/candidate.routes";
 import { errorHandler } from "./middleware/handle-errors.middleware";
 import { managerRouter } from "./routes/manager.route";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import { swaggerOptions } from '../swagger';
 
 const app = express()
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 app.use(json())
 app.use(cors({
@@ -13,6 +17,8 @@ app.use(cors({
    origin: "http://localhost:5173",
    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 }))
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(candidateRouter, managerRouter)
 
